8a8d9b273e6265902bfc320ae423c176af8310aa
trgianglotus <hoangtruonggiangnguyen@gmail.com>
2020-05-03 04:52:26 +0800

    compentize contest list, add styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import ContestPreview from "./ContestPreview";
+import ContestList from "./ContestList";
 import PropTypes from "prop-types";
 
 class App extends React.Component {
@@ -16,11 +16,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header message={this.state.pageHeader} />
-        <div>
-          {this.state.contests.map((contest) => (
-            <ContestPreview {...contest} key={contest.id} />
-          ))}
-        </div>
+        <ContestList contests={this.state.contests} />
       </div>
     );
   }
--- src/components/ContestPreview.js
+++ src/components/ContestPreview.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-const ContestPreview = (contest) => (
-  <div className="ContestPreview">
-    <div className="category-name">{contest.categoryName}</div>
-    <div className="contest-name">{contest.contestName}</div>
-  </div>
-);
+export default class ContestPreview extends Component {
+  handleClick = () => {
+    console.log(this.props.categoryName);
+  };
 
-export default ContestPreview;
+  render() {
+    return (
+      <div className="link ContestPreview" onClick={this.handleClick}>
+        <div className="category-name">{this.props.categoryName}</div>
+        <div className="contest-name">{this.props.contestName}</div>
+      </div>
+    );
+  }
+}
+
+ContestPreview.propTypes = {
+  categoryName: PropTypes.string.isRequired,
+  contestName: PropTypes.string.isRequired,
+};
